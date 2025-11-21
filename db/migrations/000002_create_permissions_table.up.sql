ba243a405824fe832df70d3255995bdafd4acc8d
William Koller <williamkoller30@gmail.com>
2025-11-21 18:29:33 -0300

    feat: add migration create permissions table

---
+++ db/migrations/000002_create_permissions_table.up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS permissions (
+    id SERIAL PRIMARY KEY,
+    user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+    modules TEXT[] NOT NULL,
+    actions TEXT[] NOT NULL,
+    level TEXT NOT NULL CHECK (level IN ('allowed', 'restricted', 'denied')),
+    description TEXT,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
+    deleted_at TIMESTAMP DEFAULT NULL
+);
